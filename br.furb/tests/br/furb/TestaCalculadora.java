341b66cfead3906c2474d091d1176c7ad9a4f74e
algoedert <alison.lierti@gmail.com>
2016-06-15 00:53:00 -0300

    parcial teste unitario

--- br.furb/tests/br/furb/TestaCalculadora.java
+++ br.furb/tests/br/furb/TestaCalculadora.java
@@ -11,12 +11,15 @@ import fpa.model.Contribuicao;
 import fpa.model.FaixaComplexidade;
 import fpa.model.Funcao;
 import fpa.model.Projeto;
+import fpa.start.StartupBean;
 
 public class TestaCalculadora {
 
 	private Contribuicao contribuicao;
 	private Funcao funcao;
-	private FaixaComplexidade faixaComplexidade;
+	private FaixaComplexidade faixaComplexidadeALI;
+	private int tiposDeDado;
+	private int tiposDeRegistro;
 
 	@Before
 	public void setUp() throws Exception {
@@ -26,6 +29,8 @@ public class TestaCalculadora {
 		contribuicao.setBaixa(7);
 		contribuicao.setMedia(10);
 		contribuicao.setAlta(15);
+		tiposDeDado = 10;
+		tiposDeRegistro = 15;
 		
 		funcao = new Funcao();
 		funcao.setHoras(4);
@@ -34,8 +39,12 @@ public class TestaCalculadora {
 		projeto.setValorHora(new BigDecimal(100));
 		funcao.setProjeto(projeto);
 		
-		faixaComplexidade = new FaixaComplexidade();
-//		faixaComplexidade.setIntervaloDados(intervaloDados);
+		FaixaComplexidade faixaComplexidadeALI = new FaixaComplexidade();
+		FaixaComplexidade faixaComplexidadeAIE = new FaixaComplexidade();
+		FaixaComplexidade faixaComplexidadeSE = new FaixaComplexidade();
+		FaixaComplexidade faixaComplexidadeCE = new FaixaComplexidade();
+		FaixaComplexidade faixaComplexidadeEE = new FaixaComplexidade();
+		StartupBean.carregaDados(faixaComplexidadeALI, faixaComplexidadeAIE, faixaComplexidadeSE, faixaComplexidadeCE, faixaComplexidadeEE);
 	}
 
 	@After
@@ -46,7 +55,7 @@ public class TestaCalculadora {
 	@Test
 	public void test() {
 		
-		FPACalculator.calculaValorFuncao(funcao, faixaComplexidade, contribuicao, tiposDeDado, tiposDeRegistro)
+		FPACalculator.calculaValorFuncao(funcao, faixaComplexidadeALI, contribuicao, tiposDeDado, tiposDeRegistro);
 	}
 
 }
